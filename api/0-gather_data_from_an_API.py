439e8ef97dba6fb4c1a4dbc89b48102c36010749
wubeshetA <w.yimam@alustudent.com>
2022-10-20 10:52:16 +0300

    hot fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,9 +25,9 @@ def main():
 
     user_name = requests.get(user_url).json()['name']
 
-    printer = ("""Employee {} is done with tasks({:}/{}):
-    """.format(user_name, len(completed), total_questions))
-    print(printer, end='')
+    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
+               len(completed), total_questions))
+    print(printer)
     for q in completed:
         print("\t {}".format(q))
 
